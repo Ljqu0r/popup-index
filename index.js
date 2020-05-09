4209ef7b417cfce0fe507d2bd9814e7c8b5b9b70
Ljqu0r <Ljqu0r@gmail.com>
2020-05-09 22:51:57 +0300

    new js and some data-attr in html

--- index.js
+++ index.js
@@ -26,6 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
     // popupShow(popupBtn, popup);
     // popupHide(popupClose, popup);
 
+    // ________________________________________________________________________________________________
+
+    // var modalOpenBtn = document.querySelectorAll(".js-btn-popup");
+    // var modalPopup = document.querySelectorAll(".js-popup-wrapper");
+    // var modalCloseBtn = document.querySelectorAll(".js-popup-close");
+
+    // function popupArr(element, func) {
+    //     for (var i = 0; i < element.length; i++) {
+    //         element[i].addEventListener("click", func);
+    //     }
+    // }
+
+    // popupArr(modalOpenBtn, modalShow);
+
+    // function modalShow() {
+    //     for (var i = 0; i < modalPopup.length; i++) {
+    //         if (modalPopup[i].classList.contains("js-popup--hidden")) {
+    //             modalPopup[i].classList.remove("js-popup--hidden");
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    // function modalClose() {
+    //     for (var i = 0; i < modalPopup.length; i++) {
+    //         if (!modalPopup[i].classList.contains("js-popup--hidden")) {
+    //             modalPopup[i].classList.add("js-popup--hidden");
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    // popupArr(modalCloseBtn, modalClose);
+
     var modalOpenBtn = document.querySelectorAll(".js-btn-popup");
     var modalPopup = document.querySelectorAll(".js-popup-wrapper");
     var modalCloseBtn = document.querySelectorAll(".js-popup-close");
@@ -36,27 +72,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    popupArr(modalOpenBtn, modalShow);
-
     function modalShow() {
-        for (var i = 0; i < modalPopup.length; i++) {
-            if (modalPopup[i].classList.contains("js-popup--hidden")) {
-                modalPopup[i].classList.remove("js-popup--hidden");
-            } else {
-                return false;
-            }
-        }
+        var data = this.getAttribute("data-open");
+        document.querySelector(`.js-popup-wrapper[data-${data}="${data}"]`).classList.remove("js-popup--hidden");
     }
 
     function modalClose() {
-        for (var i = 0; i < modalPopup.length; i++) {
-            if (!modalPopup[i].classList.contains("js-popup--hidden")) {
-                modalPopup[i].classList.add("js-popup--hidden");
-            } else {
-                return false;
-            }
-        }
+        var data = this.getAttribute("data-close");
+        document.querySelector(`.js-popup-wrapper[data-${data}="${data}"]`).classList.add("js-popup--hidden");
     }
 
+    popupArr(modalOpenBtn, modalShow);
     popupArr(modalCloseBtn, modalClose);
 });
